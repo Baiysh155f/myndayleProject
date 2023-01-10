e7d021db37ea18e529526dbc44aad4fb18f069c3
admin <baiysh.zmb@gmail.com>
2023-01-10 22:17:54 +0600

    initial

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("lksdnfkjnasdknfklasnfnd");
+        System.out.printf("lksdnfkjnasdknfklasnfnd");
+        System.out.printf("lksdnfkjnasdknfklasnfnd");
+    }
+}
